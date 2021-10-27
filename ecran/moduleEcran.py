62702f5adc83869f13d648668efc3ac051c8dc03
Steven BODERE <steven.bodere@.fr>
2021-10-28 00:27:08 +0200

    2021-10-28 - SBE - post

--- ecran/moduleEcran.py
+++ ecran/moduleEcran.py
@@ -79,29 +79,33 @@ class ModuleEcran (threading.Thread):
     "Module de gestion de l'écran"
     def __init__(self, debug):
         self.debug=debug
-        self.mngAffichage = Affichage()
-        self.mngAffichage.start()
+        self.dataAPI = []
+        self.lastData = 0
+        self.infoCOnfig= ""
+        self.mngAffichage = Affichage() ################################
+        self.mngAffichage.start() ################################
+
 
         color1=graphics.Color(255, 255, 255)
         color2=graphics.Color(255, 255, 0)
         color3=graphics.Color(255, 255, 0)
         #Création de l'écran
-        self.scrbus = Ecran("01",1,True)
-        self.scrbus .addLigne(Ligne("Ville Arret",7,0,0,color1))#Titre
-        self.scrbus .addLigne(Ligne("XX",14,0,0,color2))#ligne
-        self.scrbus .addLigne(Ligne("Direction",14,12,0,color2))#Destination
-        self.scrbus .addLigne(Ligne("5mn",14,50,0,color2))#Temps
-        self.scrbus .addLigne(Ligne("XX",21,0,0,color2))
-        self.scrbus .addLigne(Ligne("Direction",21,12,0,color2))
-        self.scrbus .addLigne(Ligne("5mn",21,50,0,color2))
-        self.scrbus .addLigne(Ligne("XX",29,0,0,color2))
-        self.scrbus .addLigne(Ligne("Direction",29,12,0,color2))
-        self.scrbus .addLigne(Ligne("5mn",29,50,0,color2))
+        self.scrbus = Ecran("Bus",1,True)
+        self.scrbus.addLigne(Ligne("Ville Arret",7,0,0,color1))#Titre
+        self.scrbus.addLigne(Ligne("",14,0,0,color2))#ligne
+        self.scrbus.addLigne(Ligne("",14,12,0,color2))#Destination
+        self.scrbus.addLigne(Ligne("",14,50,0,color2))#Temps
+        self.scrbus.addLigne(Ligne("",21,0,0,color2))
+        self.scrbus.addLigne(Ligne("",21,12,0,color2))
+        self.scrbus.addLigne(Ligne("",21,50,0,color2))
+        self.scrbus.addLigne(Ligne("",29,0,0,color2))
+        self.scrbus.addLigne(Ligne("",29,12,0,color2))
+        self.scrbus.addLigne(Ligne("",29,50,0,color2))
 
         #Création de l'écran Bienvenu
         self.scrWelcome = Ecran("IP",10,False)
         self.scrWelcome.addLigne(Ligne("@IP : ",7,0,0,color2))
-        self.scrWelcome.addLigne(Ligne(getIp(),14,0,0,color2))
+        self.scrWelcome.addLigne(Ligne(self.getIp(),14,0,0,color2))
         self.scrWelcome.addLigne(Ligne("v 0.5.2",21,0,0,color2))
         self.scrWelcome.addLigne(Ligne("SVNFTg==",29,0,0,color2))
 
@@ -111,45 +115,100 @@ class ModuleEcran (threading.Thread):
         self.scrError.addLigne(Ligne("Inconnu",14,0,0,color2))
 
         #Création de l'écran Attente Config
-        self.scrWait = Ecran("Error",10,False)
-        self.scrWait.addLigne(Ligne("Attente",7,0,0,color2))
+        self.scrWait = Ecran("Wait",10,False)
+        self.scrWait.addLigne(Ligne("Wait Data",7,0,0,color2))
         self.scrWait.addLigne(Ligne("IP:",14,0,0,color2))
-        self.scrWait.addLigne(Ligne("$ ",21,0,0,color2))
+        self.scrWait.addLigne(Ligne("null ",21,0,0,color2))
         self.scrWait.addLigne(Ligne("$ ",29,0,0,color2))
 
         #Affichage sur l'écran
-        self.mngAffichage.updateAff(self.scrWelcome)
-        time.sleep(15)
+        self.mngAffichage.updateAff(self.scrWelcome) ################################
+        # self.printScr(self.scrWelcome) ################################
+        time.sleep(0.5)
         #Thread
         threading.Thread.__init__(self)
 
-    def getIp():
+    def getIp(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
         return s.getsockname()[0]
 
+    def conf(self, info):
+        self.infoCOnfig=info
+        self.scrbus.ligne[0].texte=str(info)
+
     def update(self, info):
         self.dataAPI=info
+        self.lastData=time.time()
         if self.debug == 1:
             print("ModuleEcran : Update :",info,"")
+            print("")
+
+    # def printScr(self,scr):
+    # 	if self.debug == 1:
+    #         print("Ecran", scr.name, "nbLign:",len(scr.ligne))
+    #         for ligne_current in scr.ligne:
+    #             print("    ", ligne_current.texte)
+    #         print("")
+
+    def writeTime(self,actuel,futur):
+        diffTime=float(futur-actuel)
+        ret=""
+        if diffTime <= 0:
+            return "0m"
+        else:
+            h=diffTime/3600
+            m=(diffTime-(int(h)*3600))/60
+            hi=str(int(h))
+            mi=str(int(m))
+            if h >= 1:
+                ret = hi+"h"+mi
+            else:
+                ret = mi+"m"
+            return ret
 
     def run(self):
         while 1:
             #Gestion des données TODO
             # for in self.:
             #Gestion Affichage
-            for scr_current in self.scr:
-                if scr_current.time > 0 and scr_current.actif == True:
-                    # if self.debug == 1:
-                    #     print("Ecran", scr_current.name)
-                    # for ligne_current in scr_current.ligne:
-                    #     if self.debug == 1:
-                    #         print("    ", scr_current.ligne[0].texte)
-                    #         print("")
-                    # print("updateModule")
-            self.mngAffichage.updateAff(self.scrbus)
+            tisp = time.time()
+
+            if self.lastData+3600 < tisp: # si les données sont trop ancien (plus de communication) 1h
+                self.scrWait.ligne[2].texte=self.getIp()
+                # self.printScr(self.scrWait)################################
+                self.mngAffichage.updateAff(self.scrWait) )################################
+            else:
+                pasAff = []
+                for pas in self.dataAPI:
+                    print("=>",pas["ligne"], pas["terminus"],pas["temps"])
+                    if len(pasAff) < 3 and float(pas["temps"]) > tisp:
+                        pasAff.append(pas)
+                    elif len(pasAff) >= 3:
+                        break
+
+                for idx in range(0,3):
+                    if len(pasAff) > idx:
+                        # self.scrbus.ligne[1+(idx*3)].texte="test"
+                        # self.scrbus.ligne[2+(idx*3)].texte="test"
+                        # self.scrbus.ligne[3+(idx*3)].texte="test"
+                        self.scrbus.ligne[1+(idx*3)].texte=str(pasAff[idx]["ligne"])
+                        self.scrbus.ligne[2+(idx*3)].texte=str(pasAff[idx]["terminus"])
+                        self.scrbus.ligne[3+(idx*3)].texte=self.writeTime(tisp,float(pasAff[idx]["temps"]))
+                    else:
+                        self.scrbus.ligne[1+(idx*3)].texte=""
+                        self.scrbus.ligne[2+(idx*3)].texte=""
+                        self.scrbus.ligne[3+(idx*3)].texte=""
+
+                if len(pasAff) == 0:
+                    self.scrbus.ligne[1].texte="Pas de bus"
+
+                # self.printScr(self.scrbus)################################
+                self.mngAffichage.updateAff(self.scrbus) )################################
             print("refresh")
-            time.sleep(0.5)
+            time.sleep(5)
+
+
 
 
 # Main function
